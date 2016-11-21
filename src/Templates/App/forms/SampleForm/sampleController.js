36aa7eb424aee078a46812b54cdb7564cf140e00
Denis Molodtsov <Denis Molodtsov>
2016-11-21 00:38:59 -0500

    added function for generationg sample angular forms

---
+++ src/Templates/App/forms/SampleForm/sampleController.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+app.controller('MainCtrl', function ($scope, $http, $log, $location, spFormFactory) {    
+    $scope.spFormFactory = spFormFactory;
+
+    spFormFactory.initialize($scope, 'LIST_TITLE').then(init);
+    $scope.calendarFormat = 'dd-MMMM-yyyy';   
+
+    $scope.saveWithButtonDisabled = function(){
+        $scope.hideSaveButton = 'true';
+        $scope.save();
+    }
+
+    function init() {
+        //Business logic goes here:
+   
+    }
+
+});
--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
+"use strict";
 var fs = require('fs');
 var path = require('path');
 var csomapi = require('csom-node');
 var deferred = require('deferred');
-
-var spr = require('sp-request').create({
-        'username': 'user name',
-        'password': 'password',
-    });
+var ncp = require('ncp').ncp;
+var camelCase = require('lodash.camelcase');
 
 var spforms = spforms || {};
 
 spforms.init = function(settings) {
-    var _self = this;
+    var _self = {};
     _self.settings = settings;
 
     // Add CEWP to all List forms 
     _self.addCEWP2LitstForms = function(webPartSettings) {
-        var siteRelativeUrl = str = '/' +  webPartSettings.siteUrl.replace(/^(?:\/\/|[^\/]+)*\//, "");
+        var siteRelativeUrl = '/' +  webPartSettings.siteUrl.replace(/^(?:\/\/|[^\/]+)*\//, "");
 
         csomapi.setLoaderOptions({url: webPartSettings.siteUrl});  //set CSOM library settings
         var authCtx = new AuthenticationContext(webPartSettings.siteUrl);
@@ -117,10 +115,131 @@ spforms.init = function(settings) {
 
             
         });
-    };
+    };// addCEWP2ListForms end
+
+    //Get SP Fields from the list
+    _self.getListFields = function(listSettings){
+        var credentialOptions = {
+            'username': _self.settings.username,
+            'password': _self.settings.password,
+        };
+
+        function initializeField(result) {
+            var retVal = {};
+            retVal.Id = result.Id;
+            retVal.FieldDisplayName = result.Title;
+            retVal.FieldInternalName = result.InternalName;
+            retVal.FieldType = result.TypeAsString;
+            retVal.Required = result.Required;
+            retVal.ReadOnlyField = result.ReadOnlyField;
+            if (result.Choices) {
+                retVal.Choices = result.Choices.results;
+            }
+            //TODO: handle multichoice
+            //TODO: handle lookups
+            //TODO: handle taxonomy
+
+            return retVal;
+        };
+
+        var spr = require('sp-request').create(credentialOptions);
+
+        var listFieldsUrl = listSettings.siteUrl +
+         "/_api/web/lists/GetByTitle('" + listSettings.listTitle + "')/fields?$filter=Hidden eq false";
+        
+        return spr.get(listFieldsUrl)
+        .then(function (response) {
+            var results = response.body.d.results;
+            var f = {};
+            for (var x = 0; x < results.length; x++) {
+                if (!results[x].Hidden) {
+                    if (results[x].InternalName != 'ContentType') {
+                        if (results[x].InternalName != 'Attachments') {
+                            var field = initializeField(results[x]);
+                            f[results[x].InternalName] = field;                
+                        }
+                    }
+                }
+            }
+            //returning data to the promise
+            return f;
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+    }
+
+    //Read SharePoint list metadata and generate JSON file based on it
+    _self.generateAngularForm = function(listSettings){
+        _self.getListFields(listSettings)
+        .then(function(){
+            //TODO: generate JSON with all field types?
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+
+    //Copy scaffolding for the Angular forms.
+    //Copy all files if not copied
+    _self.copyForms = function(copySettings){
+        ncp('./node_modules/spforms/src/Templates/App', copySettings.destinationFolder,{clobber:false}, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+
+            var listFormFolderName = camelCase(copySettings.listTitle);
+
+            var listFormFolder = copySettings.destinationFolder + '/forms/' + listFormFolderName;
+
+            ncp(//Copy Folder:
+                copySettings.destinationFolder + '/forms/SampleForm', 
+                listFormFolder,
+                { clobber:false },
+                _=>{ 
+                    //onFolderCopied:
+                    fs.readdir(listFormFolder, (err, files) => {
+                        files.forEach(file => {
+                            let oldFilePath = listFormFolder +'/' + file;
+                            let newFilePath = listFormFolder +'/' + file.replace('sample', listFormFolderName);
+                            fs.rename(oldFilePath, newFilePath,  _=>{
+                                let replacementSettings = copySettings;
+                                replacementSettings.file = newFilePath;
+                                _self.replaceTokensInFile(copySettings);   
+                            }
+                            );
+                        });
+                    });
+                }
+            );
+        });
+    }
+
+    _self.replaceTokensInFile = function(replacementSettings){
+        if(replacementSettings.file.indexOf('sample') != -1){
+            return;
+        }
+
+        var tokens2Replace = new Map(); 
+        tokens2Replace.set(/DEPLOYMENT_FOLDER/g, replacementSettings.deploymentFolder);
+        tokens2Replace.set(/LIST_TITLE/g, replacementSettings.listTitle);
+        tokens2Replace.set(/LIST_NAME/g, camelCase(replacementSettings.listTitle));    
+        
+        var epta = replacementSettings.file;
+
+        var data = fs.readFileSync(epta, 'utf8');
+           
+        tokens2Replace.forEach(function(value, key) {
+            data  = data.replace(key, value);
+        });
+
+        fs.writeFileSync(epta, data, 'utf8');
+    }
 
     return _self;
 }
+
+
 
 
 module.exports = spforms.init;
